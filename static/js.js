3ef029b1e67315608ba7080bf17a3de1462d8a5e
Lukas Meitz <lukasmariusmeitz@gmail.com>
2020-12-07 20:21:10 +0100

    added the <div> structure into main html element

--- static/js.js
+++ static/js.js
@@ -8,6 +8,10 @@ var offset = 0;
 window.addEventListener('wheel', function(event)
 {
 
+    // these are our parameters
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
     event.preventDefault();
     event.stopPropagation();
 
@@ -15,7 +19,12 @@ window.addEventListener('wheel', function(event)
     var text_box = document.getElementById('text_box');
     var background_box = document.getElementById('background_box');
 
-    offset -= event.deltaY * 20;
+    // offset -= event.deltaY * 20;
+    if(event.deltaY > 0) {
+        offset -= window.innerHeight;
+    } else {
+        offset += window.innerHeight;
+    }
 
     image_box.style.transform = "translateY(" + offset + "px)";
     text_box.style.transform = "translateY(" + (offset * 1.5) + "px)";
@@ -24,36 +33,15 @@ window.addEventListener('wheel', function(event)
 });
 
 
-
-// make the second slide div
-function make_slide_2() {
-
-    var root_div = document.createElement("div");
-
-
-
-}
-
-
 // make the slideshow
 function make_slides_from_data() {
 
     // these are our resources
     var background = "Background.png";
-    var data = ["",
-                "PLASTIK: DIE UNSICHTBAREN GEFAHREN",
-                "WAS MACHT PLASTIK SO GEFÄHRLICH FÜR UNS?",
-                ""];
-     var images = ["", "Kugel_1.png", "Kolben_Rot.png", ""];
-
-
-    // either width or height should always be at min 100 %
-    // already solved: set background mode to div-contain
 
     // these are our parameters
     var w = window.innerWidth;
     var h = window.innerHeight;
-
 
     // we start by preparing our scroll boxes, one for each moving layer
     var background_box = document.getElementById('background_box');
@@ -68,28 +56,26 @@ function make_slides_from_data() {
     background.style.height = '5760px';
     background_box.appendChild(background);
 
+    // id list
+    var ids = ["kolben_1", "kolben_2", "maennchen_1", "maennchen_2"];
 
     // loop through our prepared data to generate a slideshow
     var i;
-    for(i = 0; i < data.length; i++){
+    for(i = 0; i < ids.length; i++){
 
-        // create a background slide element
-        var slide_field = document.createElement("div");
-        slide_field.style.backgroundImage = "url('" + images[i] + "')";
+        // scale the image´s parent div
+        var slide_field = document.getElementById(ids[i]);
         slide_field.style.width = w + 'px';
         slide_field.style.height = h + 'px';
-        image_box.appendChild(slide_field);
 
-        // create a text field element in the overlay
-        var text_field = document.createElement("div");
-        text_field.innerHTML = data[i];
+        // scale the text and its parent div
+        var text_field = document.getElementById(ids[i] + "_text");
         text_field.style.width = w + 'px';
         if( i == 0) {
             text_field.style.height = (h * 1.25 ) + 'px';
         } else {
             text_field.style.height = (h * 1.5) + 'px';
         }
-        text_box.appendChild(text_field);
 
     }
 
